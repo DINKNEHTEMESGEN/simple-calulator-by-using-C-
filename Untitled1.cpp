13cbcb6d2332af3a63d4128db41013319b602e5c
DINKNEHTEMESGEN <gjhomdinknehtemesgen@gmial.com>
2024-07-26 12:14:41 +0300

    this is the beggning

---
+++ Untitled1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"*************************************\n";
+	cout<<"This is simple calculator\n";
+	cout<<"*************************************\n";
+	
+		cout<<"Here are choices to calculate\n";
+	int calculator;
+	switch (calculator){
+		case'1': 	cout<<"Enter two numbers to ADD\n";
+		break;
+		case'2':cout<<"Enter two numbers to SUBTRACT\n";
+			break;
+		case'3':cout<<"Enter two numbers to MULTIPLY\n";
+			break;
+		case'4':cout<<"Enter two numbers to DIVIDE\n";
+			break;
+	default:break;
+	}
+		
+}
